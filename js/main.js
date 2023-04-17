cf7541c0687b5fb32e3ab8d777802b71c8aa3a56
OvsyankinVI <717ovsyankin@gmail.com>
2023-04-17 23:54:02 +0500

    230417_Add cards

--- js/main.js
+++ js/main.js
@@ -41,3 +41,123 @@ breakpoints: {
     },
   } 
 });
+
+var swiper1 = new Swiper(".mySwiper", {
+  direction: "vertical",
+  // loop: true,
+  autoplay: {
+    delay: 1500,
+  },
+  autoplayTimeout: 2000,
+  slidesPerView: 3,
+  spaceBetween: 24,
+  speed: 700,
+
+});
+
+let activeBackSlider = document.querySelector(".active-slider");
+let tabs = document.querySelectorAll(".ft");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    activeBackSlider.style.left = `${tab.offsetLeft}px`;
+    tabs.forEach((t) => {
+      t.classList.remove("active");
+    });
+    tab.classList.add("active");
+  });
+});
+
+var slide4 = document.getElementById("slide4");
+var slide5 = document.getElementById("slide5");
+var slide6 = document.getElementById("slide6");
+let sliders = [slide4, slide5, slide6];
+let img = Array.from(document.querySelectorAll(".bar-child"));
+
+document.getElementById("btn4").addEventListener("click", function () {
+  if (slide4.style.top === "-100%") {
+    slide4.style.transitionDelay = "600ms";
+    slide5.style.transitionDelay = "400ms";
+    slide6.style.transitionDelay = "200ms";
+    slide6.style.top = "156px";
+    slide6.style.width = "741.96px";
+    slide5.style.top = "80px";
+    slide4.style.top = "0%";
+    removeActive(sliders);
+    slide4.classList.add("active");
+    img[0].src = "img/polygon-62.svg"
+    img[1].src = "img/polygon-63.svg"
+    img[2].src = "img/polygon-63.svg"
+    /* activeBackSlider.style.width = "33%"; */
+  } else {
+    slide4.style.transitionDelay = "200ms";
+    slide5.style.transitionDelay = "400ms";
+    slide6.style.transitionDelay = "600ms";
+  }
+    // slide1.style.top = "-100%";
+    // slide2.style.top = "-100%";
+    // slide3.style.top = "-200%";
+          
+});
+
+document.getElementById("btn5").addEventListener("click", function () {
+  if (slide5.style.top === "-100%") {
+    slide4.style.transitionDelay = "600ms";
+    slide5.style.transitionDelay = "400ms";
+    slide6.style.transitionDelay = "200ms";
+    slide4.style.top = "-100%";
+    slide6.style.top = "116px";
+    slide6.style.width = "824.4px";
+    slide5.style.top = "0%";
+    removeActive(sliders);
+    slide5.classList.add("active");
+    img[0].src = "img/polygon-63.svg";
+    img[1].src = "img/polygon-62.svg";
+    img[2].src = "img/polygon-63.svg";
+    /* activeBackSlider.style.width = "33%"; */
+
+  } else {
+    slide4.style.transitionDelay = "200ms";
+    slide5.style.transitionDelay = "400ms";
+    slide6.style.transitionDelay = "600ms";
+    slide5.style.top = "0%";
+    slide4.style.top = "-100%";
+    slide6.style.top = "116px";
+    slide6.style.width = "824.4px";
+    removeActive(sliders);
+    slide5.classList.add("active");
+    img[0].src = "img/polygon-63.svg"
+    img[1].src = "img/polygon-62.svg"
+    img[2].src = "img/polygon-63.svg"
+  }
+});
+
+document.getElementById("btn6").addEventListener("click", function () {
+  if (slide6.style.top !== "0%") {
+    slide4.style.transitionDelay = "200ms";
+    slide5.style.transitionDelay = "400ms";
+    slide6.style.transitionDelay = "600ms";
+    slide6.style.top = "0%";
+    slide6.style.width = "916px";
+    slide4.style.top = "-100%";
+    slide5.style.top = "-100%";
+    removeActive(sliders);
+    slide6.classList.add("active");
+    img[0].src = "img/polygon-63.svg"
+    img[1].src = "img/polygon-63.svg"
+    img[2].src = "img/polygon-62.svg"
+    /* activeBackSlider.style.width = "35%"; */
+  } else {
+    // slide3.style.top = "0%";
+    // slide1.style.top = "-100%";
+    // slide2.style.top = "-200%";
+    // slide3.style.transitionDelay = "200ms";
+  }
+  
+});
+
+function removeActive(arr) {
+  arr.forEach((a) => {
+    a.classList.remove("active");
+  });
+};
